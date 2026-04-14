bd72a6b944dd90b954ca78eb259ed2ac7d7934dc
Tejas272226 <tejasgowdagm272@gmail.com>
2026-04-14 20:55:58 +0530

    Added headless chrome support

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,18 +3,23 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        WebDriver driver=new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.practicetestautomation.com/practice-test-login/");
         driver.manage().window().maximize();
+
         driver.findElement(By.id("username")).sendKeys("student");
         driver.findElement(By.id("password")).sendKeys("Password123");
         driver.findElement(By.id("submit")).click();
